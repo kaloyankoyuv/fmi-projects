2d62b03765b4cc308b5efa8ca9116150f3ff605b
Kaloyan Koyuv <kaloyankoyuv@gmail.com>
2023-12-31 11:24:04 +0200

    formatting code

--- Introduction-to-programming-2023-2024/homework01/2MI0800389_2_1.cpp
+++ Introduction-to-programming-2023-2024/homework01/2MI0800389_2_1.cpp
@@ -2,58 +2,55 @@
 
 double countHW(char *schedule) {
 
-	int i = 0;
-	double result = 0;
+  int i = 0;
+  double result = 0;
 
-	while(schedule[i] != 0) {
-		if(schedule[i]=='w') {
-			result++;
-		}
-		i++;
-	}
+  while (schedule[i] != 0) {
+    if (schedule[i] == 'w') {
+      result++;
+    }
+    i++;
+  }
 
-	return result;
-
+  return result;
 }
 
 double countST(char *schedule) {
 
-	int i = 0;
-	double result = 0;
+  int i = 0;
+  double result = 0;
 
-	while(schedule[i] != 0) {
-		if(schedule[i]=='s') {
-			result++;
-		}
-		i++;
-	}
+  while (schedule[i] != 0) {
+    if (schedule[i] == 's') {
+      result++;
+    }
+    i++;
+  }
 
-	return result;
-
+  return result;
 }
 
 int main() {
 
+  int numberOfDays = 5;
 
-	int numberOfDays = 5;
+  double N;
+  std::cin >> N;
 
-	double N;
-	std::cin >> N;
+  double hoursInvested = 0;
 
-	double hoursInvested = 0;
+  char schedule[255];
 
-	char schedule[255];
+  while (numberOfDays--) {
+    std::cin >> schedule;
+    hoursInvested += countHW(schedule) + countST(schedule) / 2;
+  }
 
-	while(numberOfDays--) {
-		std::cin >> schedule;
-		hoursInvested += countHW(schedule) + countST(schedule)/2;
-	}
+  if (hoursInvested >= N) {
+    std::cout << "You got it!" << std::endl;
+  } else {
+    std::cout << "You should work more" << std::endl;
+  }
 
-	if(hoursInvested >= N) {
-		std::cout << "You got it!" << std::endl;
-	} else {
-		std::cout << "You should work more" << std::endl;
-	}
-
-	return 0;
+  return 0;
 }
